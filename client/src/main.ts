4953f0725fbd6c6ada43eb92817b120e2ce5fd97
mrdapoyo <wiiuforever25@gmail.com>
2024-07-20 16:17:36 +0200

    a

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,24 +1,12 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { socket } from "./socket";
+import { pc } from "./webrtc";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let remoteStream: MediaStream | null = null;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let callId = new URLSearchParams(window.location.search).get("id");
+let userId = crypto.randomUUID();
+
+if (!callId) {
+  callId = crypto.randomUUID();
+  window.history.pushState("", "", `/?id=${callId}`);
+}
