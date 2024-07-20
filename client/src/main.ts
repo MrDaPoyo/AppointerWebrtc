4081fe10596077a9ae82c35432d212af1d413b28
mrdapoyo <wiiuforever25@gmail.com>
2024-07-20 16:45:32 +0200

    a

--- client/src/main.ts
+++ client/src/main.ts
@@ -10,3 +10,47 @@ if (!callId) {
   callId = crypto.randomUUID();
   window.history.pushState("", "", `/?id=${callId}`);
 }
+
+const webcamVideo = document.getElementById("webcam-video") as HTMLVideoElement | null;
+const remoteVideo = document.getElementById("remote-video") as HTMLVideoElement | null;
+
+webcamVideo && (webcamVideo.muted = true);
+
+navigator.mediaDevices.getUserMedia(
+  { 
+    video: true, 
+    audio: true 
+  }
+)
+.then(async (localStream) => {
+  remoteStream = new MediaStream();
+
+  localStream.getTracks().forEach((track) => {
+    pc.addTrack(track, localStream);
+  });
+  pc.ontrack = (event) => {
+    remoteStream?.addTrack(event.track);
+  
+    console.log(event);
+
+  };
+  if (webcamVideo) webcamVideo.srcObject = localStream;
+  if (remoteVideo) remoteVideo.srcObject = remoteStream;
+
+  socket.emit("join-room", { callId, userId });
+});
+
+socket.on("user-connected", async () => {
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+
+  socket.emit("call-offer", userId, offer);
+});
+
+socket.on("user-offer", async (uid, offer) => {
+  pc.setRemoteDescription(new RTCSessionDescription(offer));
+  const answer = await pc.createAnswer();
+  await pc.setLocalDescription(answer);
+
+  socket.emit("call-answer", userId, answer);
+});
